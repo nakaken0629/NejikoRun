32300e73152a6728ebc48868815dc52dfa748ddc
NAKAGAKI Kenji <nakaken0629@gmail.com>
2017-07-09 22:00:07 +0900

    スコアとライフの表示処理を作成

---
+++ Assets/Scripts/LifePanel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifePanel : MonoBehaviour
+{
+	public GameObject[] icons;
+
+	// ライフに応じてスプライトを出し分ける
+	public void UpdateLife (int life)
+	{
+		for (int i = 0; i < 3; i++) {
+			if (i < life) {
+				icons [i].SetActive (true);
+			} else {
+				icons [i].SetActive (false);
+			}
+		}
+	}
+}
