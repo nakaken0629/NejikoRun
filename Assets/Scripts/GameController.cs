3d232ea4dc7a84311ca32f470e0b366ec5cc7003
NAKAGAKI Kenji <nakaken0629@gmail.com>
2017-07-10 21:47:25 +0900

    タイトル画面の作成

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -18,11 +18,26 @@ public class GameController : MonoBehaviour
 
 		// ライフパネルを更新
 		lifePanel.UpdateLife (nejiko.Life ());
+
+		// ねじ子のライフが０になったらゲームオーバー
+		if (nejiko.Life () <= 0) {
+			// これ以降のUpdateは止める
+			enabled = false;
+
+			// 2秒後にReturnToTitleを呼び出す
+			Invoke ("ReturnToTitle", 2.0f);
+		}
 	}
 
 	int CalcScore ()
 	{
 		// ねじ子の走行距離をスコアとする
 		return (int)nejiko.transform.position.z;
+	}
+
+	void ReturnToTitle ()
+	{
+		// タイトルシーンに切り替え
+		Application.LoadLevel ("Title");
 	}
 }
---
+++ Assets/Scripts/TitleController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleController : MonoBehaviour
+{
+	public void OnStartButtonClicked ()
+	{
+		Application.LoadLevel ("Main");
+	}
+}
